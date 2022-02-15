6e82629d12dd5c90852fb469ebc650cc84448612
NoCodeNoBlunder <findrunas@hotmail.com>
2022-02-15 20:44:57 +0100

    fixed pick-tasks html

--- start_dev.ps1
+++ start_dev.ps1
@@ -1,6 +1,5 @@
 $curDir = Get-Location
-Start-Job -WorkingDirectory $curDir//MicroShop//micro-shop-frontend {npm run start:dev}
-Start-Job -WorkingDirectory $curDir//MicroShop//micro-shop-backend {npm run start:dev}
-Start-Job -WorkingDirectory $curDir//MicroWarehouse//micro-warehouse-frontend {npm run start:dev}
-Start-Job -WorkingDirectory $curDir//MicroWarehouse//micro-warehouse-backend {npm run start:dev}
-
+Start-Job -Name "shopFrontend"  -WorkingDirectory $curDir//MicroShop//micro-shop-frontend {npm run start:dev}
+Start-Job -Name "shopBackend" -WorkingDirectory $curDir//MicroShop//micro-shop-backend {npm run start:dev}
+Start-Job -Name "wareFrontend" -WorkingDirectory $curDir//MicroWarehouse//micro-warehouse-frontend {npm run start:dev}
+Start-Job -Name "WareBackend" -WorkingDirectory $curDir//MicroWarehouse//micro-warehouse-backend {npm run start:dev}
